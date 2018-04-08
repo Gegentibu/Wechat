265146e9365b7d219dfa2157954cdf66593ad3de
Gegentibu <474714440@qq.com>
2018-04-08 17:39:48 +0800

    4月8

--- pages/discover/supply/supply.js
+++ pages/discover/supply/supply.js
@@ -5,16 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    add_img: [],
+    textarea: '',
+    input:''
   },
-  data: {
-    add_img: []
+  bindInputBlur:function (e){
+    this.setData({
+      input: e.detail.value
+    })
+  },
+  bindTextAreaBlur: function (e) {
+    this.setData({
+      textarea: e.detail.value
+    })
   },
   gotoShow: function () {
     var that = this;
     wx.chooseImage({
       success: function (res) {
-        // console.log(res.tempFilePaths)
+        // console.log(res)
         var src = res.tempFilePaths;
         var aa = that.data.add_img.concat(src)
         // console.log(aa)
@@ -50,7 +59,45 @@ Page({
       urls: this.data.add_img
     })
   },
+  uploadImg:function(e){
+ 
+      var that = this;
+      var filePath = that.data.add_img;
+      console.log(filePath)
+      for (var i = 0, h = filePath.length; i < h; i++) {
+        wx.uploadFile({
+          url: 'https://api.mongoliaci.com/api/wechat/image', //开发者服务器 url
+          filePath: filePath[i],//要上传文件资源的路径
+          name: 'file', //文件对应的 key , 开发者在服务器端通过这个 key 可以获取到文件二进制内容
+          formData: { //HTTP 请求中其他额外的 form data
+            'user': 'test'
+          },
+          success: function (res) {
+            var data = res.data
+            // var data = JSON.parse(res.data);
+            //do something
+            console.log(res)
 
+          }
+        })
+      }
+
+      wx.request({
+        url: 'https://api.mongoliaci.com/api/wechat/image', //仅为示例，并非真实的接口地址
+        data: {
+          x: that.data.textarea,
+          y: that.data.input
+        },
+        method:'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded' // 默认值
+        },
+        success: function (res) {
+          console.log(res.data)
+        }
+      })
+    
+  },
   /**
    * 生命周期函数--监听页面加载
    */
